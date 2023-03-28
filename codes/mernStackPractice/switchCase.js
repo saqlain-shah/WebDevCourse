d3c49c127241e74f3d11c3e138b86cd4639d149d
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-29 04:26:18 +0500

    change file name to switchCase

--- codes/mernStackPractice/switchCase.js
+++ codes/mernStackPractice/switchCase.js
@@ -1,12 +1,15 @@
-let a = 2 + 2;
+let sum = 2 + 2;
 
-switch (a) {
-  case 3:
+switch (sum) {
+  case 1:
     console.log( 'Too small' );
+break;
   case 4:
     console.log( 'Exactly!' );
-  case 5:
+    break;
+  case 8:
     console.log( 'Too big' );
+    break
   default:
     console.log( "I don't know such values" );
 }
