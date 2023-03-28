fe997d2f00a46f93bc8568934a17f324922f8ea0
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-29 04:13:16 +0500

    change file name to ifElse

---
+++ codes/mernStackPractice/ifElse.js
@@ -0,0 +1,13 @@
+//let year = console.log('In which year was ECMAScript-2015 specification published?', '');
+let year=2015;
+// if (year == 2015) 
+// console.log( 'You are right!' );
+// let y = console.log('In which year was the ECMAScript-2015 specification published?', '');
+
+if (year == 2015) {
+  console.log( 'Wow! Its correct' );
+} 
+else {
+  console.log( 'Ops! Try again' ); // any value except 2015
+}
+
