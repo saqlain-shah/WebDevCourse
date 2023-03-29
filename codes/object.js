ec228f44e9d9453abc89107c41f8bc8e98cf0c32
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-29 21:46:29 +0500

    class task on object method

--- codes/object.js
+++ codes/object.js
@@ -6,3 +6,13 @@ let trainee=
        Interest: ['Programming','Reading','Treking','Gaming']
     }
 console.log(trainee);
+
+function Person(first, last, age, height) {
+    this.firstName = "Shehbaz";
+    this.lastName= "Ali";
+    this.age = 24;
+    this.height = 5.6;
+  }
+  const person1=new Person();
+  console.log(person1);
+  
