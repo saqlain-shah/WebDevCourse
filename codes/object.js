ad563e4182ae89deeae9a5cdcc61d6eb30c9e234
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-29 21:38:15 +0500

    pratice on object method

---
+++ codes/object.js
@@ -0,0 +1,8 @@
+let trainee=
+    {
+       name:"Shehbaz Ali",
+       qualification:"BS Computer Science",
+       age:24,
+       Interest: ['Programming','Reading','Treking','Gaming']
+    }
+console.log(trainee);
