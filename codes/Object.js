3788f3d972312ce2343fc140eda74d4e90b505ab
ZAKIR ANSARI <zakiransari068@gmail.com>
2023-03-29 22:18:59 +0500

    Now jason parse method is used to convert jason to object format

--- codes/Object.js
+++ codes/Object.js
@@ -10,13 +10,28 @@ console.log(`Your Intrest in `,Bio.Intrest);
 let jasonformat=JSON.stringify(Bio);
 console.log(jasonformat);
 
-// Object Constructor
-// function MyInfo(name,age,Intrest){
-//     this.name=`Zakir`,
-//     this.age=25,
-//     this.Intrest=[`programming`,`reading`]
-//     return this.Intrest;
+//Object this Method
+function MyInfo(){
+    this.name=`Zakir`,
+    this.age=25,
+    this.Intrest=[`programming`,`reading`]
+    
+}
+const self=new MyInfo();
+console.log(self)
 
-// }
+// Jason to object converter
+let data={
+   
+    Name: `Ansari`,
+    Qualification : `BSCS`,
+    Intrest :[`Games`,`Reading`,`travelling`],
+    Age : 25,
+} 
+let obj=JSON.stringify(data); // converting to jason
+console.log(obj);
+let result=JSON.parse(obj) ; // converting jason to object
+console.log(result);
+
 // let Info2=new MyInfo(`Shehbaz`,24,[`Treking`,`Reading`]);
 // console.log(Info2.Intrest);
