376448fffa52232814b1fcf32cfe35963576ca0e
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-29 22:18:38 +0500

    Convert object into JSON Using stringify method and JSON into object uisng parse method

--- codes/object.js
+++ codes/object.js
@@ -5,7 +5,15 @@ let trainee=
        age:24,
        Interest: ['Programming','Reading','Treking','Gaming']
     }
-console.log(trainee);
+console.log("\n convert obect into JSON using stringify method ");
+
+    //convert obect into JSON using stringify method
+    const myJSON = JSON.stringify(trainee);
+console.log(myJSON);
+console.log("\n convert JSON into object");
+  //convert JSON into object 
+  const jsonParse = JSON.parse(myJSON);
+  console.log(jsonParse);
 
 function Person(first, last, age, height) {
     this.firstName = "Shehbaz";
