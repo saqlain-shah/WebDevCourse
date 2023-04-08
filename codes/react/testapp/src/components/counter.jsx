e1abd16adfd9530586d9996b8db2a8a7ec91188f
Tezeeb hassan <tehzeebhassan01@gmail.com>
2023-04-08 22:34:04 +0500

    react life cycle

--- codes/react/testapp/src/components/counter.jsx
+++ codes/react/testapp/src/components/counter.jsx
@@ -11,21 +11,25 @@ export default function MyButton() {
       setCount(count + 1);
       if (count==10)
       setCount(count)
+      console.log(count)
     }
     function countDecrease() {
         setCount(count -1);
         if(count==0){
             setCount(count)
+           
             
         }
+        console.log(count)
       }
     return (
     <div class="new">
     <Button   variant="contained" color="success" onClick={countIncrease}>plus </Button>
-   
+    
   
       <Button variant="contained" color="error" onClick={countDecrease}>Subtract</Button>
       <p class="cot">{count}</p>
+    
     
     </div>  
     )};
