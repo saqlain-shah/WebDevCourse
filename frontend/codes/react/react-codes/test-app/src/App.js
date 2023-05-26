f2a34d83024275375c59f63a174992458c104d58
Malika Bano <mtmalikatarannum@gmail.com>
2023-05-25 23:50:07 -0700

    backend files

--- frontend/codes/react/react-codes/test-app/src/App.js
+++ frontend/codes/react/react-codes/test-app/src/App.js
@@ -15,15 +15,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './component/loginFormpage';
 import RegistrationForm from './component/registrationForm';
 import SuccessfulPage from "./component/successfulPage";
+import Header from "./header";
 
 const App = () => {
   return (<BrowserRouter>
     <Routes>
       <Route path="/" element={<registrationForm />}/>
         <Route index element={<LoginPage />} />
+        <Route index element={<header />} />
         <Route path="/" element={<successfulPage />}/>
         <Route path="RegistrationForm" element={<RegistrationForm />} /> 
         <Route path="LoginPage" element={<LoginPage />} />
+        <Route path="Header" element={<Header />} />
         <Route path="SuccessfulPage" element={<SuccessfulPage />} />  
     </Routes>
   </BrowserRouter>
