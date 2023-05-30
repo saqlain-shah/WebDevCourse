124126c1af28d397edaad14c51f8403787a4cb28
aliya <aliyacqv786>
2023-05-30 16:12:50 +0500

    added files

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,7 @@
 import express from "express";
-import signUp from "../controllers/auth";
+import { signUp } from "../controllers/auth.js";
 
 const router = express.Router();
 router.post("/sign-up", signUp);
+
+export default router;
