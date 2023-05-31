abdd99e9d871af309d947d33306903c7c05ec980
Fatima hadi <fatimahadi151214@gmail.com>
2023-05-31 08:45:07 -0700

    add backend files(like server.js)

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+
+dotenv.config();
+
+const app = express();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_ATLAS);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
+// Middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api", authRoute);
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, async () => {
+  connect();
+  console.log(`Server listening on port ${PORT}`);
+  console.log("Express Server is started.");
+});
