4edc09ff952a25e898139c6a4fa2e2de0d3f4233
mehmooda <mehmoodakiran9@gmail.com>
2023-06-05 16:46:54 -0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,21 @@ import cors from "cors";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
 
+
 const app = express();
 dotenv.config();
 
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_ATLAS);
-    console.log("Connected to mongoDB.");
-  } catch (error) {
-    throw error;
-  }
+const connect = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_ATLAS);
+        console.log("Connected to mongoDB.");
+    } catch (error) {
+        throw error;
+    }
 };
 
 mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected!");
+    console.log("mongoDB disconnected!");
 });
 
 //middlewares
@@ -30,18 +31,18 @@ app.use("/api", authRoute);
 app.use("/api/users", usersRoute);
 
 app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong!";
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
 });
 
 app.listen(process.env.PORT, () => {
-  connect();
-  console.log(`Server listen on port ${process.env.PORT}`);
-  console.log("Express Server is Started.");
+    connect();
+    console.log(`Server listen on port ${process.env.PORT}`);
+    console.log("Express Server is Started.");
 });
