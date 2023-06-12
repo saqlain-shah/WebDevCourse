60141ecc2e275e9be4d09fc8cb3153586990a83f
aliya <aliyacqv786>
2023-06-12 16:21:38 +0500

    rooms routes

---
+++ backend/routes/roomsRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/roomsController.js";
+//import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+//CREATE
+router.post("/:hotelid", createRoom);
+
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", deleteRoom);
+//GET
+
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getRooms);
+
+export default router;
+
+
+
