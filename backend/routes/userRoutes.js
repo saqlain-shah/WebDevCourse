47cec0a923449416d3b1d3be982e6c291e55345b
aliya <aliyacqv786>
2023-06-15 17:19:55 +0500

    user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,18 +4,36 @@ import {
   deleteUser,
   getUser,
   getUsers,
-  Requests,
 } from "../controllers/userController.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+ router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+   res.send("hello user, you are logged in")
+ })
+
+router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+  res.send("hello user, you are logged in and you can delete your account")
+})
+
+ router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+   res.send("hello admin, you are logged in and you can delete all accounts")
+ })
+
 //UPDATE
-router.post("/:id", Requests);
-router.put("/:id", updateUser);
-router.put("/", updateUser);
+router.put("/:id", verifyUser, updateUser);
+
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
+
 //GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
+
 //GET ALL
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
+
 export default router;
+
+
+
